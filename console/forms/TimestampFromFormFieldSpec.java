593cd83e4e7b77658b34982dde2669aa1b9abfca
James Pharaoh <james@pharaoh.uk>
2014-11-19 12:57:51 +0000

    manual responder simple report

---
+++ console/forms/TimestampFromFormFieldSpec.java
@@ -0,0 +1,33 @@
+package wbs.platform.console.forms;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import wbs.framework.application.annotations.PrototypeComponent;
+import wbs.framework.data.annotations.DataAttribute;
+import wbs.framework.data.annotations.DataClass;
+import wbs.platform.console.spec.ConsoleModuleData;
+
+@Accessors (fluent = true)
+@Data
+@DataClass ("timestamp-from-field")
+@PrototypeComponent ("timestampFromFormFieldSpec")
+@ConsoleModuleData
+public
+class TimestampFromFormFieldSpec {
+
+	@DataAttribute
+	String label;
+
+	@DataAttribute
+	String name;
+
+	@DataAttribute
+	Boolean nullable;
+
+	@DataAttribute
+	Boolean readOnly;
+
+	@DataAttribute
+	Integer size = FormField.defaultSize;
+
+}
