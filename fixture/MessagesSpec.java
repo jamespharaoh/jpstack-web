6eb621a02484e204478a1f4864673f9e76f02e38
Jose Luis Moreno Duran <joseluis@wellbehavedsoftware.com>
2015-05-04 09:38:59 +0000

    Im Chat message template database fixtures from xml file.

---
+++ fixture/MessagesSpec.java
@@ -0,0 +1,36 @@
+package wbs.services.messagetemplate.fixture;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import wbs.framework.data.annotations.DataAttribute;
+import wbs.framework.data.annotations.DataChildren;
+import wbs.framework.data.annotations.DataClass;
+import wbs.framework.data.annotations.DataParent;
+
+@Accessors (fluent = true)
+@Data
+@DataClass ("messages")
+public 
+class MessagesSpec {
+
+	@DataParent
+	ImChatMessageTemplateDatabaseSpec messageTemplateDatabaseElements;
+
+	@DataAttribute
+	String type;
+
+	@DataAttribute
+	String name;
+
+	@DataChildren (
+		direct = true,
+		childElement = "param",
+		keyAttribute = "name",
+		valueAttribute = "value")
+	Map<String,String> params =
+		new LinkedHashMap<String,String> ();
+	
+}
