446ae3de164bf96d5514eb5b3ddbdc2113588a65
James Pharaoh <james@pharaoh.uk>
2015-10-04 19:27:34 +0000

    create model meta xml and migrate priv types to it

--- core/model/ticket-data.sql
+++ core/model/ticket-data.sql
@@ -1,10 +1,3 @@
-SELECT priv_type_insert (
-	'ticket_manager',
-	'manage',
-	'Full control',
-	'Full control of this Ticket Manager',
-	true);
-
 SELECT queue_type_insert (
 	'ticket_state',
 	'default',
