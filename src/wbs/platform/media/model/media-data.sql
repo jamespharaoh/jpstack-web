c1f5a5cb53b9f79f031031350baa72d31b378dbc
James Pharaoh <james@pharaoh.uk>
2015-10-05 18:17:24 +0000

    move default media types from sql into fixture

--- src/wbs/platform/media/model/media-data.sql
+++
@@ -1,9 +0,0 @@
-SELECT media_type_insert ('text/plain', 'Plain text', 'txt');
-
-SELECT media_type_insert ('image/jpeg', 'JPEG image', 'jpg');
-SELECT media_type_insert ('image/gif', 'GIF image', 'gif');
-SELECT media_type_insert ('image/png', 'PNG image', 'png');
-SELECT media_type_insert ('image/mp4', 'MPEG-4 image', 'mp4');
-
-SELECT media_type_insert ('video/3gpp', '3GPP video', '3gp');
-SELECT media_type_insert ('video/mpeg', 'MPEG video', '3gp');
