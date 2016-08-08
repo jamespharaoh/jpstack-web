7b81960c5e4c556c2fe29037d1746a4d7ff92b13
James Pharaoh <james@pharaoh.uk>
2016-08-08 11:09:06 +0000

    refactor lots of stuff from misc, move simulator to new sender type

--- console/MessageTemplateEntryValueFieldsProvider.java
+++ console/MessageTemplateEntryValueFieldsProvider.java
@@ -1,6 +1,6 @@
 package wbs.services.messagetemplate.console;
 
-import static wbs.framework.utils.etc.Misc.stringFormat;
+import static wbs.framework.utils.etc.StringUtils.stringFormat;
 
 import java.util.ArrayList;
 import java.util.List;
