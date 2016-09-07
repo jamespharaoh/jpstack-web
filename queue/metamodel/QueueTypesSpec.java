5c88d0959d6288b9c0bbf7665b058632fe317973
James Pharaoh <james@pharaoh.uk>
2016-09-07 10:25:04 +0200

    implement @WeakSingletonDependency and replace a large chunk of @Inject annotations with the custom ones

--- queue/metamodel/QueueTypesSpec.java
+++ queue/metamodel/QueueTypesSpec.java
@@ -9,7 +9,7 @@ import lombok.experimental.Accessors;
 import wbs.framework.application.annotations.PrototypeComponent;
 import wbs.framework.data.annotations.DataChildren;
 import wbs.framework.data.annotations.DataClass;
-import wbs.framework.entity.meta.ModelMetaData;
+import wbs.framework.entity.meta.model.ModelMetaData;
 
 @Accessors (fluent = true)
 @Data
