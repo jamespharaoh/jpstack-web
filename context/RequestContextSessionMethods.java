d656f014844802d30c486c79312a7e32b7295d88
James Pharaoh <james@pharaoh.uk>
2017-01-26 13:03:14 +0100

    fixes for requestContext.session() changes

--- context/RequestContextSessionMethods.java
+++ context/RequestContextSessionMethods.java
@@ -1,11 +1,15 @@
 package wbs.web.context;
 
+import static wbs.utils.etc.Misc.requiredValue;
 import static wbs.utils.etc.OptionalUtils.optionalFromNullable;
+import static wbs.utils.etc.OptionalUtils.optionalGetRequired;
+import static wbs.utils.etc.OptionalUtils.optionalIsPresent;
 import static wbs.utils.etc.OptionalUtils.optionalOrEmptyString;
 import static wbs.utils.etc.TypeUtils.genericCastUnchecked;
 import static wbs.utils.etc.TypeUtils.genericCastUncheckedNullSafe;
 
 import java.io.Serializable;
+import java.util.function.Supplier;
 
 import javax.servlet.http.HttpSession;
 
@@ -30,6 +34,52 @@ interface RequestContextSessionMethods
 			genericCastUncheckedNullSafe (
 				session ().getAttribute (
 					key)));
+
+	}
+
+	default
+	Serializable sessionRequired (
+			@NonNull String key) {
+
+		return requiredValue (
+			genericCastUncheckedNullSafe (
+				session ().getAttribute (
+					key)));
+
+	}
+
+	default
+	Serializable sessionOrElseSetRequired (
+			@NonNull String key,
+			@NonNull Supplier <? extends Serializable> supplier) {
+
+		Optional <Serializable> valueOptional =
+			optionalFromNullable (
+				genericCastUncheckedNullSafe (
+					session ().getAttribute (
+						key)));
+
+		if (
+			optionalIsPresent (
+				valueOptional)
+		) {
+
+			return optionalGetRequired (
+				valueOptional);
+
+		} else {
+
+			Serializable value =
+				requiredValue (
+					supplier.get ());
+
+			session ().setAttribute (
+				key,
+				value);
+
+			return value;
+
+		}
 
 	}
 
