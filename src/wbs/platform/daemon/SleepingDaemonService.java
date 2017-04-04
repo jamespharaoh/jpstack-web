8339d2c474c90d0029bb0e26ea8283174f2887ae
James Pharaoh <james@pharaoh.uk>
2017-04-04 13:56:16 +0200

    more fixes for sleeping daemon process

--- src/wbs/platform/daemon/SleepingDaemonService.java
+++ src/wbs/platform/daemon/SleepingDaemonService.java
@@ -337,7 +337,7 @@ class SleepingDaemonService
 				backgroundProcess.getRunning ()
 
 				&& earlierThan (
-					backgroundProcess.getRunningStartTime (),
+					backgroundProcess.getRunningWatchdogTime (),
 					transaction.now ().minus (
 						Duration.standardMinutes (10)))
 
