e2df7a600b6fbb3f0564905c3b73ee236eb085b7
James Pharaoh <james@pharaoh.uk>
2017-04-07 11:05:52 +0000

    make daemon runner terminate once per hour automatically

--- daemon/DaemonRunner.java
+++ daemon/DaemonRunner.java
@@ -1,31 +1,66 @@
 package wbs.platform.daemon;
 
+import static wbs.utils.time.TimeUtils.earlierThan;
+
 import java.util.List;
 
 import lombok.NonNull;
-import lombok.extern.log4j.Log4j;
 
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+
+import wbs.framework.component.annotations.ClassSingletonDependency;
+import wbs.framework.component.annotations.SingletonDependency;
+import wbs.framework.logging.LogContext;
 import wbs.framework.logging.TaskLogger;
+
+import wbs.utils.random.RandomLogic;
 
 import info.faljse.SDNotify.SDNotify;
 
-@Log4j
 public
 class DaemonRunner {
 
-	public static
+	// singleton dependencies
+
+	@ClassSingletonDependency
+	LogContext logContext;
+
+	@SingletonDependency
+	RandomLogic randomLogic;
+
+	// implementation
+
+	public
 	void runDaemon (
 			@NonNull TaskLogger parentTaskLogger,
 			@NonNull List <String> arguments)
 		throws InterruptedException {
 
-		log.info ("Daemon started");
+		TaskLogger taskLogger =
+			logContext.nestTaskLogger (
+				parentTaskLogger,
+				"runDaemon");
+
+		taskLogger.noticeFormat (
+			"Daemon started");
 
 		SDNotify.sendNotify ();
 
 		try {
 
-			for (;;) {
+			Instant restartTime =
+				Instant.now ().plus (
+					restartFrequency.getMillis ()
+					- restartFrequencyDeviation.getMillis ()
+					+ randomLogic.randomInteger (
+						restartFrequencyDeviation.getMillis () * 2));
+
+			while (
+				earlierThan (
+					Instant.now (),
+					restartTime)
+			) {
 
 				Thread.sleep (1000);
 
@@ -33,12 +68,28 @@ class DaemonRunner {
 
 			}
 
+			taskLogger.noticeFormat (
+				"Automatic periodic restart");
+
 		} finally {
 
-			log.info ("Daemon shutting down");
+			taskLogger.noticeFormat (
+				"Daemon shutting down");
 
 		}
 
 	}
+
+	// constants
+
+	public final static
+	Duration restartFrequency =
+		Duration.standardHours (
+			1l);
+
+	public final static
+	Duration restartFrequencyDeviation =
+		Duration.standardMinutes (
+			5l);
 
 }
