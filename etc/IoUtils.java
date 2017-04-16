2bb842caec01b4e4cc00547e9ce6974ef59ef579
James Pharaoh <james@pharaoh.uk>
2017-04-16 16:26:50 +0200

    remove more log4j in favour of tasklogger

--- etc/IoUtils.java
+++ etc/IoUtils.java
@@ -1,6 +1,30 @@
 package wbs.utils.etc;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
+import lombok.NonNull;
+
 public
 class IoUtils {
+
+	public static
+	void writeBytes (
+			@NonNull OutputStream target,
+			@NonNull byte[] bytes) {
+
+		try {
+
+			target.write (
+				bytes);
+
+		} catch (IOException ioException) {
+
+			throw new RuntimeException (
+				ioException);
+
+		}
+
+	}
 
 }
