aa785e9ef48ce60fa97407a138532511d5595ec2
James Pharaoh <james@pharaoh.uk>
2017-05-02 12:42:20 +0000

    refactor to remove activity manager, make task logger auto closeable, reduce use of thread locals in general, use task logger in many more places

--- fixture/WalletFixtureProvider.java
+++ fixture/WalletFixtureProvider.java
@@ -5,11 +5,11 @@ import lombok.NonNull;
 import wbs.framework.component.annotations.ClassSingletonDependency;
 import wbs.framework.component.annotations.PrototypeComponent;
 import wbs.framework.component.annotations.SingletonDependency;
-import wbs.framework.database.OwnedTransaction;
+import wbs.framework.database.NestedTransaction;
+import wbs.framework.database.Transaction;
 import wbs.framework.entity.record.GlobalId;
 import wbs.framework.fixtures.FixtureProvider;
 import wbs.framework.logging.LogContext;
-import wbs.framework.logging.TaskLogger;
 
 import wbs.platform.menu.model.MenuGroupObjectHelper;
 import wbs.platform.menu.model.MenuItemObjectHelper;
@@ -54,24 +54,24 @@ class WalletFixtureProvider
 	@Override
 	public
 	void createFixtures (
-			@NonNull TaskLogger parentTaskLogger,
-			@NonNull OwnedTransaction transaction) {
+			@NonNull Transaction parentTransaction) {
 
 		try (
 
-			TaskLogger taskLogger =
-				logContext.nestTaskLogger (
-					parentTaskLogger,
+			NestedTransaction transaction =
+				parentTransaction.nestTransaction (
+					logContext,
 					"createFixtures");
 
 		) {
 
 			menuItemHelper.insert (
-				taskLogger,
+				transaction,
 				menuItemHelper.createInstance ()
 
 				.setMenuGroup (
 					menuGroupHelper.findByCodeRequired (
+						transaction,
 						GlobalId.root,
 						"test",
 						"facility"))
@@ -98,11 +98,12 @@ class WalletFixtureProvider
 
 			WalletServiceRec walletService =
 				walletServiceHelper.insert (
-					taskLogger,
+					transaction,
 					walletServiceHelper.createInstance ()
 
 				.setSlice (
 					sliceHelper.findByCodeRequired (
+						transaction,
 						GlobalId.root,
 						"test"))
 
@@ -118,7 +119,7 @@ class WalletFixtureProvider
 			);
 
 			walletHelper.insert (
-				taskLogger,
+				transaction,
 				walletHelper.createInstance ()
 
 				.setWalletService (
