fd10c6fb013edc29bd0d69b8c02e05f551a937f6
James Pharaoh <james@pharaoh.uk>
2017-08-01 10:18:58 +0000

    work on shopify webhook synchronisation

--- exceptions/HttpUnprocessableEntityException.java
+++ exceptions/HttpUnprocessableEntityException.java
@@ -1,6 +1,12 @@
 package wbs.web.exceptions;
 
+import static wbs.utils.collection.CollectionUtils.emptyList;
+import static wbs.utils.etc.OptionalUtils.optionalOf;
+import static wbs.utils.etc.OptionalUtils.optionalOr;
+
 import java.util.List;
+
+import com.google.common.base.Optional;
 
 import lombok.NonNull;
 
@@ -12,13 +18,26 @@ class HttpUnprocessableEntityException
 
 	public
 	HttpUnprocessableEntityException (
-			@NonNull String statusMessage,
+			@NonNull Optional <String> statusMessage,
 			@NonNull List <String> errors) {
 
 		super (
 			HttpStatus.httpUnprocessableEntity,
-			statusMessage,
+			optionalOr (
+				statusMessage,
+				"Unprocessable entity"),
 			errors);
+
+	}
+
+	public
+	HttpUnprocessableEntityException (
+			@NonNull String statusMessage) {
+
+		this (
+			optionalOf (
+				statusMessage),
+			emptyList ());
 
 	}
 
